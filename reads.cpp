33bef6ff3c47512cf4950e789cb45b12db8c5d13
Josué Ttito <josue.ttito@ucsp.edu.pe>
2019-10-04 18:24:12 -0500

    add reads

--- reads.cpp
+++ reads.cpp
@@ -7,6 +7,8 @@
 #include <chrono>
 #include <ctime>
 
+#include <typeinfo>
+
 
 using namespace std;
 
@@ -14,12 +16,19 @@ using namespace std;
 int main(int argc, char** argv) {
     srand (time(NULL));
     typedef Interval<int> Tinterval;
+    int a_size = 1000;
 
     int key_domain_size = 10000000;
     int leaf_size = 1000000;
     int R = 1500000;
     int query_number;
     map <Tinterval, string> results;
+
+    double a_raw_time = 0;
+    double a_tree_time = 0;
+    double a_query_time = 0;
+    int a_leafs_size = 0;
+    int a_extra_insertion = 0;
 
     if (argc != 2) {
         cout << "Please enter an initial size" << endl;
@@ -29,7 +38,7 @@ int main(int argc, char** argv) {
     sscanf(argv[1], "%d", &query_number);
 
 
-    for (int k = 0; k < 1; k++) {
+    for (int k = 0; k < a_size; k++) {
         vector <Tinterval > intervals;
 
         rocksdb::DB* db;
@@ -49,7 +58,9 @@ int main(int argc, char** argv) {
             intervals.push_back(I);
         }
         auto start_time = std::chrono::system_clock::now();
-        for (auto & interval: intervals) {
+
+        for (int i = 0; i < intervals.size(); i++) {
+            Tinterval interval = intervals[i];
             std::string start = to_string(interval.left);
             std::string limit = to_string(interval.right);
 
@@ -62,20 +73,23 @@ int main(int argc, char** argv) {
         }
         auto end_time = std::chrono::system_clock::now();
         std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+        a_raw_time += elapsed_seconds.count();
         // cout << elapsed_seconds.count() << ",";
 
 
-        int extra_insertions = 0;
+        // int extra_insertions = 0;
         start_time = std::chrono::system_clock::now();
-        double sum_total = 0;
+        // double sum_total = 0;
         Tree <int> tree(leaf_size);
         for (auto & interval: intervals) {
-            extra_insertions += tree.insert_interval(interval);
+            tree.insert_interval(interval);
         }
         end_time = std::chrono::system_clock::now();
 
         elapsed_seconds = end_time - start_time;
-        sum_total += elapsed_seconds.count();
+        // double tree_time =
+        // sum_total += elapsed_seconds.count();
+        a_tree_time += elapsed_seconds.count();
         // cout << elapsed_seconds.count() << ",";
 
         start_time = std::chrono::system_clock::now();
@@ -90,24 +104,49 @@ int main(int argc, char** argv) {
             for (it->Seek(start);
                 it->Valid() && it->key().ToString() < limit;
                 it->Next()) {
-                    // results[interval] = it->value().ToString();
-                    // std::cout << it->key().ToString() << ": " << it->value().ToString() << std::endl;
+                    results[interval] = it->key().ToString();
             }
-            // cout << "--> " << start << " * " << limit << endl;
 
+        }
+
+        for (int i = 0; i < intervals.size(); i++) {
+            Tinterval interval = intervals[i];
+            results[interval];
         }
         end_time = std::chrono::system_clock::now();
 
         elapsed_seconds = end_time - start_time;
-        sum_total += elapsed_seconds.count();
+        a_query_time += elapsed_seconds.count();
+
+        a_leafs_size += leafs.size();
+        a_extra_insertion += tree.number_pending;
+        // sum_total += elapsed_seconds.count();
         // cout << elapsed_seconds.count() << ",";
         // cout << sum_total << ",";
         // cout << leafs.size() << ",";
-        // cout << extra_insertions << "\n";
+        // cout << tree.number_pending << "\n";
 
         delete it;
 
         delete db;
     }
 
+    a_raw_time = a_raw_time / a_size;
+    a_tree_time = a_tree_time / a_size;
+    a_query_time = a_query_time / a_size;
+    a_leafs_size = a_leafs_size / a_size;
+    a_extra_insertion = a_extra_insertion / a_size;
+    // double a_raw_time = 0;
+    // double a_tree_time = 0;
+    // double a_query_time = 0;
+    // double a_total_time = 0;
+    // int a_leafs_size = 0;
+    // int a_extra_insertion = 0;
+
+    cout << a_raw_time << ",";
+    cout << a_tree_time << ",";
+    cout << a_query_time << ",";
+    cout << a_tree_time + a_query_time << ",";
+    cout << a_leafs_size << ",";
+    cout << a_extra_insertion << endl;
 }
